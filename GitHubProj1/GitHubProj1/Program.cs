d10a75d7105dfd0daadf0ece7ec25e74fa80285a
John Fallon <fallonj@gmail.com>
2013-04-14 18:47:16 +0100

    First Hello world committ

---
+++ GitHubProj1/GitHubProj1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHubProj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github!");
+            Console.ReadKey();
+        }
+    }
+}

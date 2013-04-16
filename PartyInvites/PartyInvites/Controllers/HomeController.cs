9105e7abe5ab7cbf6ce20016acace0006a26abd4
John Fallon <fallonj@gmail.com>
2013-04-16 19:00:47 +0100

    End chapter 3 - data validation & model binding

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -20,9 +21,25 @@ namespace PartyInvites.Controllers
             return View();        
         }
 
+        [HttpGet]
         public ViewResult RsvpForm() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email guestResponse to the party organiser
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //There is a validation error - redisplay the form
+                return View();
+            }    
         }
 
     }

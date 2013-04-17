33c4f4d855ce2c3197932b2adc1c564d6d0921fb
John Fallon <fallonj@gmail.com>
2013-04-17 23:21:19 +0100

    First Moq example

--- UnitTestingDemo/UnitTestingDemo.Tests/MyPriceReducerTest.cs
+++ UnitTestingDemo/UnitTestingDemo.Tests/MyPriceReducerTest.cs
@@ -3,12 +3,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Moq;
 
 namespace UnitTestingDemo.Tests
 {
     [TestClass]
     public class MyPriceReducerTest
     {
+        private IEnumerable<Product> products;
+
+        [TestInitialize]
+        public void PreTestInitialise() 
+        {
+            // Arrange
+            products = new Product[] {
+                new Product() { Name = "Kayak", Price = 275M},
+                new Product() { Name = "Lifejacket", Price = 48.95M},
+                new Product() { Name = "Soccer ball", Price = 19.50M},
+                new Product() { Name = "Stadium", Price = 79500M}
+            };
+        }
+
 
         [TestMethod]
         public void All_Prices_Are_Changed() {
@@ -35,16 +50,21 @@ namespace UnitTestingDemo.Tests
         [TestMethod]
         public void Correct_Total_Reduction_Amount()
         {
-            // Arrange
-            FakeRepository repo = new FakeRepository();
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m=> m.GetProducts()).Returns(products);
+
+            //FakeRepository repo = new FakeRepository();
             decimal reductionAmount = 10;
-            decimal initialTotal = repo.GetTotalValue();
-            MyPriceReducer target = new MyPriceReducer(repo);
+            decimal initialTotal = products.Sum(x=>x.Price);
+            MyPriceReducer target = new MyPriceReducer(mock.Object);
             // Act
             target.ReducePrices(reductionAmount);
+
             // Assert
-            Assert.AreEqual(repo.GetTotalValue(),
-            (initialTotal - (repo.GetProducts().Count() * reductionAmount)));
+            Assert.AreEqual(
+                                products.Sum(p => p.Price),
+                                (initialTotal - (products.Count() * reductionAmount))
+                            );
         }
 
         [TestMethod]

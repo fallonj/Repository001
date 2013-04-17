5ae4e2ffc35cca7ab3a088caf3054519eadcb4c4
John Fallon <fallonj@gmail.com>
2013-04-17 23:58:43 +0100

    New project for general practice

---
+++ GeneralPractice/GeneralPractice.Tests/BikeTest.cs
@@ -0,0 +1,33 @@
+﻿using GeneralPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GeneralPractice.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for BikeTest and is intended
+    ///to contain all BikeTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BikeTest
+    {
+
+        [TestMethod]
+        public void Test_Milage_Increased()
+        { 
+            //Arrange
+            Bike johnsBike = new Bike("Red", 2) { HasALight = false };
+            int originalMilesOnBike = johnsBike.MilesOnTheClock;
+            int journeyMiles = 40;
+
+            //Act
+            johnsBike.GoForARide(journeyMiles);
+
+            //Assert
+            Assert.AreEqual(originalMilesOnBike + journeyMiles, johnsBike.MilesOnTheClock);
+        }
+
+    }
+}
---
+++ GeneralPractice/GeneralPractice/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralPractice
+{
+    class Bike
+    {
+        public int NumberOfWheels { get; set; }
+        public bool HasALight { get; set; }
+        public string Colour { get; set; }
+        public int MilesOnTheClock { get; set; }
+
+        public void GoForARide(int HowManyMiles)
+        {
+            MilesOnTheClock += HowManyMiles;
+        }
+
+        public Bike(string colour, int numberOfWheels)
+        {
+            this.Colour = Colour;
+            this.NumberOfWheels = 2;
+        }
+
+
+    }
+}

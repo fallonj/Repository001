c89183babd6ca51f5e6c2ba0ef4cc48c8674e7f3
John Fallon <fallonj@gmail.com>
2013-04-20 09:22:07 +0100

    Displaying a List of Products

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productRepository)
+        {
+            repository = productRepository;
+        
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}

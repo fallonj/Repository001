c2e33c7b1e812548a395ff2447aaf66283ac22bd
John Fallon <fallonj@gmail.com>
2013-04-23 20:21:36 +0100

    Created an action to return distinct categories

---
+++ SportsStore/SportsStore.UnitTests/NavControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using SportsStore.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SportsStore.UnitTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for NavControllerTest and is intended
+    ///to contain all NavControllerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class NavControllerTest
+    {
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+                }.AsQueryable()
+            );
+            
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+            
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+            
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+    }
+}
--- SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
+        private IProductsRepository repository;
 
-        public string Menu()
+        public NavController(IProductsRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
         }
+
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                                                .Select(x => x.Category)
+                                                .Distinct()
+                                                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+
+
+
 
     }
 }

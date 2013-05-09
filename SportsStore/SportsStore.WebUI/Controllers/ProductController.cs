63d86c2f2d98fb4992959ee79b99e2d3f53ed1d6
John Fallon <fallonj@gmail.com>
2013-05-09 21:21:57 +0100

    Save an image per product. Stored as binary in DB
    New Image in line
    New getImage()
    Can now POST an image

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.WebUI.Models;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -50,6 +51,20 @@ namespace SportsStore.WebUI.Controllers
                                                         };
 
             return View(viewModel);
+        }
+
+
+        public FileContentResult GetImage(int productId)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
         }
 
     }
